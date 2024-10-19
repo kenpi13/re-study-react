d1fc9f41d6407ecda86108fe6a40c1a205b8fe65
kenpi13 <kenkenpi0526@gmail.com>
2024-10-19 15:46:48 +0900

    不要インポート削除

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/footer";
 
 const geistSans = localFont({
